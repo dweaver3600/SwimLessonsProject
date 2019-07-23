36e218cd55f731e3eef54109e9f76ce36cf2be68
dweaver3600 <dweaver3600@yahoo.com>
2019-07-22 23:36:56 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,31 @@ struct address {
 	string zip;
 
 	string streetAddress() {
-		return houseNumber + " " + street + "\n\t" + city + ", " + state + " " + zip;
+		return houseNumber + " " + street + "\n\t " + city + ", " + state + " " + zip;
 	}
 };
 
 class Patron {
+public:
 
-public:	
+	//Default Constructor
+	Patron() {
+		fname_ = "<FNAME>";
+		lname_ = "<LNAME>";
+
+		age_ = 0;
+
+		gender_ = '0';
+
+		phone_ = "<PHONE_NUMBER>";
+
+		address_.houseNumber = "0";
+		address_.street = "<STREET>";
+		address_.city = "<CITY>";
+		address_.state = "<STATE>";
+		address_.zip = "<ZIP_CODE>";
+	}
+
 	string fname_;
 	string lname_;
 	int age_;
@@ -29,6 +47,23 @@ public:
 
 	address address_;
 
+	Patron(string fnam, string lnam, int ag, char gen, string phn, string hnum, string strt, string cty, string state, string zip) {
+		fname_ = fnam;
+		lname_ = lnam;
+
+		age_ = ag;
+
+		gender_ = gen;
+
+		phone_ = phn;
+
+		address_.houseNumber = hnum;
+		address_.street = strt;
+		address_.city = cty;
+		address_.state = state;
+		address_.zip = zip;
+	}
+
 	// a class number to reference
 	//	create a struct for each class that exists
 	//	 - time
@@ -36,31 +71,21 @@ public:
 	//	 - pool location
 
 	void print_patron() {
-		cout << "Name:\t" << fname_ << " " << lname_ << "\n"
-			<< "Age:\t" << age_ << "\n" << "Gender:\t" << gender_ << "\n"
+		cout << "Name:\t " << fname_ << " " << lname_ << "\n"
+			<< "Age:\t " << age_ << "\n"
+			<< "Gender:\t " << gender_ << "\n"
 			<< "Address: " << address_.streetAddress();
 	}
 };
 
 int main() {
-	Patron child1;
+	Patron child2;
+	child2.print_patron();
 
-	child1.fname_ = "Charles";
-	child1.lname_ = "Denti";
+	cout << "\n\n";
 
-	child1.age_ = 7;
-
-	child1.gender_ = 'M';
-
-	child1.phone_ = "(714)979-9797";
-
-	child1.address_.houseNumber = "6776";
-	child1.address_.street = "Madup Lane";
-	child1.address_.city = "La Palma";
-	child1.address_.state = "CA";
-	child1.address_.zip = "92804";
-
-	child1.print_patron();
+	Patron child("Charles", "Denti", 7, 'M', "(714)979-9797", "6776", "Madup Lane", "La Palma", "CA", "92804");
+	child.print_patron();
 
 	PAUSE
 }
